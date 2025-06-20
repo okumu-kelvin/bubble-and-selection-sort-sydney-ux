6f1cb5b52aafa1497ae6997ae4762751622cd484
Sydney <sydney.musyoka@strathmore.edu>
2025-06-20 09:49:41 +0300

    DONE

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,16 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    
+    n = len(unsorted_list)
+    
+    # Traverse through all list elements
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # Traverse the list from 0 to n-i-1
+            # Swap if the element found is greater than the next element
+            if unsorted_list[j] > unsorted_list[j+1]:
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+    
+    return unsorted_list
+x = [30,45,7,2,34]
+print(bubble_sort(x))
